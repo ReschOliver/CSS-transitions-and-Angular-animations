2ece3ac2a40000c81bc41edd7e179f9da79dde95
ReschOliver <oliver.resch@outlook.at>
2021-10-21 08:38:13 +0200

    added routes

--- testAnimations/src/app/app-routing.module.ts
+++ testAnimations/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OpenCloseComponent } from './open-close/open-close.component';
+import { ListAnimationComponent } from "./list-animation/list-animation.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: OpenCloseComponent},
+  {path: 'buttons' ,component: OpenCloseComponent},
+  {path: 'list' ,component: ListAnimationComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
